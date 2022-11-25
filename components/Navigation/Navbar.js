afba398e7644cde11e67df417f5e508a73e27c72
edwardb@flutterint.com <edwardb@flutterint.com>
2022-11-25 14:21:45 +0000

    Translate navigation

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,27 +1,28 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Navbar.scss';
+import { useTranslation } from 'next-i18next';
 
 export default function Navbar() {
+  const { t } = useTranslation('common', { keyPrefix: 'nav' });
   const Router = useRouter();
-  const zonePages = ['/zone', '/zone/early-careers', '/zone/zone-matrix'];
 
   return (
     <>
       <style jsx>{styles}</style>
       <nav>
         <ol>
-          <li className={Router.asPath == '/' ? 'active' : ''}>
-            <Link href="/">Home</Link>
+          <li className={Router.asPath === '/' ? 'active' : ''}>
+            <Link href="/">{t('1')}</Link>
           </li>
-          <li className={Router.asPath == '/fork' ? 'active' : ''}>
-            <Link href="/fork">The Fork in the Road</Link>
+          <li className={Router.asPath === '/fork' ? 'active' : ''}>
+            <Link href="/fork">{t('2')}</Link>
           </li>
-          <li className={Router.asPath == '/know' ? 'active' : ''}>
-            <Link href="/know">Know Where You're At</Link>
+          <li className={Router.asPath === '/know' ? 'active' : ''}>
+            <Link href="/know">{t('3')}</Link>
           </li>
-          <li className={Router.asPath == '/play' ? 'active' : ''}>
-            <Link href="/play">Make Your Play</Link>
+          <li className={Router.asPath === '/play' ? 'active' : ''}>
+            <Link href="/play">{t('4')}</Link>
           </li>
         </ol>
       </nav>
