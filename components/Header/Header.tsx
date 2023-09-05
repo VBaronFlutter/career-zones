4cf6dd04bb61f0e459371d1ecd4e70be099dd7d9
V Baron <victoria.baron@hotmail.co.uk>
2023-09-05 11:21:53 +0100

    Updated styling on hompage and know page, removed old pages, broken on know

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,12 +4,26 @@ export default function Header() {
   return (
     <>
       <style jsx>{styles}</style>
+
       <header>
         <div>
           <h1>Career Zones</h1>
-          <h2>@ Internatinal Sports</h2>
-        </div>
+          <h2>I work in the 
+            <div className="autoComplete_wrapper">
+                <input id="autoComplete" placeholder="International Sports" type="search" dir="ltr" spellCheck="false" autoCorrect="off" autoComplete="off" />
+            </div>
+          tribe</h2>
+            <h2>As 
+              <select name="role" id="role-select">
+                  <option value="engineer">An Engineer</option>
+                  <option value="po">A Product Owner</option>
+                  <option value="scrum">A Scrum Master</option>
+                  <option value="seit">A Test Engineer</option>
+              </select>
+            </h2>
+          </div>
       </header>
+
     </>
   );
 }
